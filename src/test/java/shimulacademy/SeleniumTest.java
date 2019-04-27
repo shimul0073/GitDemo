d7c631a6f1d1f0a34137792a5a73e0d03397d582
shimul0073 <shossain0073@gmail.com>
2019-04-26 20:46:23 -0400

    first commit

---
+++ src/test/java/shimulacademy/SeleniumTest.java
@@ -0,0 +1,16 @@
+package shimulacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void browserAutomation() {
+		System.out.println("BrowserAutomation");
+	}
+	@Test
+	public void uiAutomation() {
+		System.out.println("BrowserAutomation");
+	}
+	
+}
